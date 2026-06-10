4d11e814ba28b9ef5782c5f7da1b7f0fa367e92f
Alexandre Santos <alexandre.santozz@gmail.com>
2026-06-10 17:09:04 +0100

    Fix library import path in cart CLI

--- library/commerce/continente/internal/cli/cart.go
+++ library/commerce/continente/internal/cli/cart.go
@@ -10,7 +10,7 @@ import (
 	"strconv"
 	"strings"
 
-	"continente-pp-cli/internal/client"
+	"github.com/mvanhorn/printing-press-library/library/commerce/continente/internal/client"
 	"github.com/spf13/cobra"
 )
 
